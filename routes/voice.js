d58d1e09e2fdbffd21db52c482503d54721ade39
Finlumina-Test <faizvnnn@gmail.com>
2025-09-27 15:25:02 +0500

    Update voice.js

--- routes/voice.js
+++ routes/voice.js
@@ -2,18 +2,18 @@
 import express from "express";
 const router = express.Router();
 
-router.post("/voice", (req, res) => {
-  const twiml = `
-    <Response>
-      <Start>
-        <Stream url="wss://${process.env.SERVER_DOMAIN}/realtime-conversation" track="inbound_track"/>
-      </Start>
-      <Say>Starting realtime conversation...</Say>
-      <Pause length="60"/>
-    </Response>
-  `;
+router.post("/", (req, res) => {
+  const wsUrl = `wss://${process.env.SERVER_DOMAIN}/realtime-conversation`;
 
-  res.type("text/xml");
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say voice="Polly.Joanna">Starting realtime conversation...</Say>
+  <Connect>
+    <Stream url="${wsUrl}" track="inbound_track" />
+  </Connect>
+</Response>`;
+
+  res.set("Content-Type", "text/xml");
   res.send(twiml);
 });
 
